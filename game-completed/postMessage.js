9f962286c4a4faeba2ed67c03779be1d71f98914
meky-vodafone <mahmoud.meky.vodafone@gmail.com>
2025-11-09 12:01:27 +0200

    new version

---
+++ game-completed/postMessage.js
@@ -0,0 +1,11 @@
+function sendGameCompleteMessageToIframeParent(caughtCharacter, timeTakenInSeconds) {
+    const message = {
+        caughtCharachter: Boolean(caughtCharacter),
+        timeTakenInSeconds: Number(timeTakenInSeconds)
+    };  
+
+    // Send message to parent window
+    if (window.parent !== window) {
+        window.parent.postMessage(message, '*');
+    }
+}
