1f0ddd7fb7bcf8b00e088bea7338c605240269b0
meky-vodafone <mahmoud.meky.vodafone@gmail.com>
2025-11-10 13:29:00 +0200

    Reset For Both Android & IOS

--- reset-btn-workaround.js
+++ reset-btn-workaround.js
@@ -1,25 +1,23 @@
-
-
 function resetModelPosition() {
-    const resetButton = document.getElementById('resetButton');
-    if (!resetButton) {
-        console.warn('Reset button not found in the DOM.');
-        return;
-    }
-    resetButton.click()
-    // alert("Model position has been reset.");
-    // resetButton.style.visibility = 'hidden';
-    // setTimeout(() => {
-    // }, 1000); // 1 second delay
-
+  const resetButton = document.getElementById("resetButton");
+  if (!resetButton) {
+    console.warn("Reset button not found in the DOM.");
+    return;
+  }
+  resetButton.click();
+  // alert("Model position has been reset.");
+  // resetButton.style.visibility = 'hidden';
+  // setTimeout(() => {
+  // }, 1000); // 1 second delay
 }
 
-// IOS Fix 
-const isIOSDevice = /iphone|ipad|ipod/i.test(navigator.userAgent);
-if (isIOSDevice) {
-    document.addEventListener('gameStarted', () => {
-        setTimeout(() => {
-            resetModelPosition();
-        }, 500);
-    })
-}
+// IOS Fix
+// const isIOSDevice = /iphone|ipad|ipod/i.test(navigator.userAgent);
+// if (isIOSDevice) {
+// ! Reset For Both Android & IOS
+document.addEventListener("gameStarted", () => {
+  setTimeout(() => {
+    resetModelPosition();
+  }, 1000);
+});
+// }
