c4bd9c98f0c18161d2fa687d8b4034b63f0f9dd1
Mekky <mahmoud.meky.vodafone@gmail.com>
2025-11-16 15:54:07 +0200

    Update gameComplete.js

--- assets/js/gameComplete.js
+++ assets/js/gameComplete.js
@@ -15,7 +15,7 @@
 
 function onGameComplete(caughtCharacter, timeTakenInSeconds) {
   //   sendGameCompleteMessageToIframeParent(caughtCharacter, timeTakenInSeconds);
-  const baseRedirectionUrl = "https://qa1.vodafone.com.eg/portal/bf/youtubePromo/game-page";
+  const baseRedirectionUrl = "https://test1.vodafone.com.eg/portal/bf/youtubePromo/game-page";
   const finalUrl = `${baseRedirectionUrl}?caughtCharacter=${caughtCharacter},timeTakenInSeconds=${timeTakenInSeconds}`;
   try {
     window.location.replace(finalUrl);
@@ -23,4 +23,5 @@ function onGameComplete(caughtCharacter, timeTakenInSeconds) {
     window.location.reload();
   }
 }
+
 
