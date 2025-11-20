892666950f4498039615d655b024eb236e3a8277
Mekky <mahmoud.meky.vodafone@gmail.com>
2025-11-20 19:23:22 +0200

    add

---
+++ assets/js/reset-btn-workaround.js
@@ -0,0 +1,28 @@
+function resetModelPosition() {
+  // const resetButton = document.getElementById("resetButton");
+  // if (!resetButton) {
+  //   console.warn("Reset button not found in the DOM.");
+  //   return;
+  // }
+  // resetButton.click();
+
+  // Another Method
+  WEBARSDK.PerformReset();
+  console.log("Model position has been reset.");
+
+  // alert("Model position has been reset.");
+  // resetButton.style.visibility = 'hidden';
+  // setTimeout(() => {
+  // }, 1000); // 1 second delay
+}
+
+// IOS Fix
+// const isIOSDevice = /iphone|ipad|ipod/i.test(navigator.userAgent);
+// if (isIOSDevice) {
+// ! Reset For Both Android & IOS
+document.addEventListener("gameStarted", () => {
+  setTimeout(() => {
+    resetModelPosition();
+  }, 1000);
+});
+// }
